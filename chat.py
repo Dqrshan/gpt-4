770bd3f6c12f87300cdcd415bc18a690b3005023
Lorenz <darshan310268@gmail.com>
2023-08-07 19:43:46 +0530

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from g4f.Provider import DeepAi
+import g4f
+import requests
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    data = request.get_json(force=True)
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=[{"role": "user", "content": data["query"]}],
+        provider=DeepAi,
+    )
+    return json.dumps({"response": response})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
