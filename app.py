8272a0288ebe030fc601aa8c2458a0552b1814b2
Lorenz <darshan310268@gmail.com>
2023-08-08 19:45:56 +0530

    feat: index page, switch provider

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, request
-from g4f.Provider import DeepAi
+from flask import Flask, request, render_template_string
+from g4f.Provider import GetGpt
 import g4f
 import json
 
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return render_template_string(
+        "<a href='https://github.com/Dqrshan/gpt-4'>Source</a>"
+    )
+
+
 @app.route("/ask", methods=["POST"])
 def ask():
     data = request.get_json(force=True)
     response = g4f.ChatCompletion.create(
-        model=g4f.models.gpt_4, messages=data["messages"], provider=DeepAi
+        messages=data["messages"], provider=GetGpt, model=g4f.models.gpt_35_turbo
     )
     return json.dumps({"response": response})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
